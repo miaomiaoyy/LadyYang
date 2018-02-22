73e7030f06d8f1aea94684e767168150f935632e
yykathleen224 <yuan.yan@husky.neu.edu>
2018-02-22 10:32:19 -0800

    fix website edit bugs

--- src/app/views/website/website-edit/website-edit.component.ts
+++ src/app/views/website/website-edit/website-edit.component.ts
@@ -3,6 +3,7 @@ import {WebsiteService} from '../../../service/website.service.client';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Website} from '../../../models/website.model.client';
 import {User} from '../../../models/user.model.client';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-website-edit',
@@ -11,12 +12,17 @@ import {User} from '../../../models/user.model.client';
 })
 export class WebsiteEditComponent implements OnInit {
 
+  @ViewChild('f') websiteForm: NgForm;
+
   website: Website;
   websites: Website[] = [];
+  userId: String;
+  websiteId: String;
 
   constructor(
     private websiteService: WebsiteService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ) { }
 
   updateWebsite() {
@@ -26,12 +32,19 @@ export class WebsiteEditComponent implements OnInit {
 
   deleteWebsite() {
     this.websiteService.deleteWebsite(this.website._id);
+    this.router.navigate(['../'], {relativeTo: this.activatedRoute});
   }
 
   ngOnInit() {
     this.activatedRoute.params.subscribe((params: any) => {
-      this.website = this.websiteService.findWebsiteById(params['websiteId']);
-      this.websites = this.websiteService.findWebsitesByUser(params['userId']);
+      this.userId = params['uid'];
+      this.websiteId = params['wid'];
+      alert('Revising website  ' + this.websiteId);
+      this.website = this.websiteService.findWebsiteById(this.websiteId);
+      this.websites = this.websiteService.findWebsitesByUser(this.userId);
+      // this.website.name = this.websiteForm.value.name;
+      // this.website.description = this.websiteForm.value.description;
+      //     this.widget.size = this.imageForm.value.headerSize;
     });
   }
 
