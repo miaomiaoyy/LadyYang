6907efa5a17c424379132bdce4e28c12d2429e83
yykathleen224 <yykathleen224@gmail.com>
2018-04-06 15:05:57 -0700

    test heruko

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 //var db = mongoose.connect('mongodb://localhost:27017/test');
 
-var db = mongoose.connect('mongodb://yang:224@ds217349.mlab.com:17349/heroku_93x3fp0h');
+var db = mongoose.connect('mongodb://admin224:yy19890224@ds217349.mlab.com:17349/heroku_93x3fp0h');
 
 module.exports = db;
 
