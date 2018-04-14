a69e95d2d73c4269f2cb522f4c0df077dcee7989
yykathleen224 <yykathleen224@gmail.com>
2018-04-13 21:58:02 -0700

    fix cake url

--- assignment/app.js
+++ assignment/app.js
@@ -3,6 +3,7 @@ module.exports = function (app){
   require("./services/website.service.server")(app);
   require("./services/page.service.server")(app);
   require("./services/widget.service.server")(app);
+  require("./services/cakes.service.server")(app);
   var db = require("./model/models.server");
 };
 
--- assignment/services/cakes.service.server.js
+++ assignment/services/cakes.service.server.js
@@ -6,10 +6,10 @@ module.exports = function (app) {
   app.post("/api/cakes/cake", createCake);
   app.get("/api/cakes", showCakes);
   app.post("api/user/cake", createCakeForUser);
-  app.get("api/user/cakes",findAllCakesForUser);
-  app.get("api/user/:cid",findCakeById);
-  app.post("api/user/cake",updateCake);
-  app.delete("/api/cakes/:cid",deleteCake);
+  app.get("api/user/cakes", findAllCakesForUser);
+  app.get("api/user/:cid", findCakeById);
+  app.post("api/user/cake", updateCake);
+  app.delete("/api/cakes/:cid", deleteCake);
 
 
   function showCakes(req, res) {
@@ -27,26 +27,25 @@ module.exports = function (app) {
     //   }
     // });
   };
-}
 
 
-function createCake(req, res) {
-  var cake = req.body;
-  var user = req.body;
+  function createCake(req, res) {
+    var cake = req.body;
+    var user = req.body;
 
     cakeModel.createCake(cake).then(function (newCake) {
       console.log("cake service1");
       res.send(newCake);
     })
-    , function (error) {
+      , function (error) {
       res.sendStatus(400).send('error');
     };
-}
+  }
 
 
-function createCakeForUser(req, res) {
-  var cake = req.body;
-  var user = req.body;
+  function createCakeForUser(req, res) {
+    var cake = req.body;
+    var user = req.body;
 
     cakeService.createCakeForUser(userId, cake)
       .then(function (cake) {
@@ -58,52 +57,52 @@ function createCakeForUser(req, res) {
   }
 
 
+  function findAllCakesForUser(req, res) {
+    var user = req.body;
+    cakeModel.findAllCakesForUser(user._id)
+      .then(
+        function (cakes) {
+          res.json(cakes);
+        },
+        function (error) {
+          res.sendStatus(400).send(error);
+        }
+      );
+  }
 
-function findAllCakesForUser(req, res) {
-  var user = req.body;
-  cakeModel.findAllCakesForUser(user._id)
-    .then(
-      function (cakes) {
-        res.json(cakes);
-      },
-      function (error) {
-        res.sendStatus(400).send(error);
-      }
-    );
+  function findCakeById(req, res) {
+    var cakeId = req.params['cid'];
+    cakeModel.findPageById(cakeId)
+      .then(
+        function (page) {
+          res.send(page);
+        },
+        function (error) {
+          res.sendStatus(400).send(error);
+        }
+      );
+  }
+
+  function updateCake(req, res) {
+    var cakeId = req.params['cid'];
+    var newCake = req.body;
+
+    cakeModel.updateCake(cakeId, newCake)
+      .then(
+        function (cake) {
+          res.json(cake);
+        },
+        function (error) {
+          res.sendStatus(400).send(error);
+        }
+      );
+  }
+
+  function deleteCake(req, res) {
+    var cakeId = req.params['cid'];
+    cakeModel.deleteCake(cakeId)
+      .then(function (status) {
+        res.send(status);
+      });
+  };
 }
-
-function findCakeById(req, res) {
-  var cakeId = req.params['cid'];
-  cakeModel.findPageById(cakeId)
-    .then(
-      function (page) {
-        res.send(page);
-      },
-      function (error) {
-        res.sendStatus(400).send(error);
-      }
-    );
-}
-
-function updateCake(req, res) {
-  var cakeId = req.params['cid'];
-  var newCake = req.body;
-
-  cakeModel.updateCake(cakeId, newCake)
-    .then(
-      function (cake) {
-        res.json(cake);
-      },
-      function (error) {
-        res.sendStatus(400).send(error);
-      }
-    );
-}
-
-function deleteCake(req, res) {
-  var cakeId = req.params['cid'];
-  cakeModel.deleteCake(cakeId)
-    .then(function (status) {
-      res.send(status);
-    });
-};
