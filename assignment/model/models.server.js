809467e6bdb381f9d882563402b5565dd83709da
yykathleen224 <yykathleen224@gmail.com>
2019-02-23 22:29:55 -0800

    Fix mongo uri

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 //var db = mongoose.connect('mongodb://localhost:27017/webdev');
 
-var db = mongoose.connect('mongodb://kathleen:yy19890224@ds061200.mlab.com:61200/ladyyang');
+var db = mongoose.connect('mongodb://kathleen224:yy19890224@ds061200.mlab.com:61200/ladyyang');
 
 module.exports = db;
 
